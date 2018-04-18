c0f0bf614fbc7e0159eafcdb8952d84d4a9ce2ec
Matias Gutierrez <matiasgutierrez@outlook.com>
2018-04-18 10:38:51 -0300

    Update 08_geometry_rendering.cpp

--- 08_geometry_rendering.cpp
+++ 08_geometry_rendering.cpp
@@ -3,7 +3,7 @@ and may not be redistributed without written permission.*/
 
 //Using SDL, SDL_image, standard IO, math, and strings
 #include<SDL2/SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string>
 #include <cmath>
